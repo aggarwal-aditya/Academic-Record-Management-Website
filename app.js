5117b553f4e92fbb651fbb9d02aa21344b1b8aa3
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2023-01-19 20:19:34 +0530

    Added mongodb database

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const session = require('express-session');
+const mongoose = require("mongoose");
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:acad@123@acaddatabase.esyhw8q.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 
